399ff068dc21de97ddd6fdd2a6450d50d2d7e1d9
icebarf <sysgrammer@protonmail.com>
2025-03-17 15:36:16 +0530

    libc: libk -> libsinkc: rename kernel libc

--- libc/stdio/putchar.c
+++ libc/stdio/putchar.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-#if defined(__is_libk)
+#if defined(__is_libsinkc)
 #include <kernel/vga_tty>
 #endif
 
 int
 putchar(int ic)
 {
-#if defined(__is_libk)
+#if defined(__is_libsinkc)
   char c = (char)ic;
   vga_term_putchar(c);
 #else
