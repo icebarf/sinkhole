618a041da5af5a599c15e39e46f3c1074ccd8930
icebarf <sysgrammer@protonmail.com>
2025-03-17 22:47:59 +0530

    kernel: test out libsinkc printf

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -3,15 +3,18 @@
 #include <kernel/serial>
 #include <kernel/vga_tty>
 
+// TODO: Issue with printing exactly the 25th line
+// output is truncated
 extern "C" void
 kernel_main()
 {
   vga_term_init();
   printf("Hello, kernel world!\n");
-  printf("no - %d\n", 25);
+  for (int i = 0; i < 23; i++)
+    printf("%d\n", i);
   int v = init_serial();
   if (v >= 1) {
-    printf("sinkhole: serial: %s with code %d\n", "initialization failed", v);
+    printf("sinkhole: serial: initialization success with code %d\n", v);
     return;
   }
   printf("sinkhole: serial: %s with code %d\n", "initialization success", v);
