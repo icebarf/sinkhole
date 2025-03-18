87f2300077a2a2c065f59fe13faf791f539a6b97
icebarf <sysgrammer@protonmail.com>
2025-03-18 23:58:16 +0530

    libc: fix warnings

--- libc/string/memmove.c
+++ libc/string/memmove.c
@@ -8,13 +8,17 @@ memmove(void* dest, const void* src, size_t n)
 
   if (_src == _dest || n == 0)
     return _dest;
-  if (_dest > _src && _dest - _src < (int)n) {
+  if (_dest > _src && _dest - _src < (long)n) {
     size_t i;
-    for (i = n - 1; i >= 0; i--)
+    // The code `n-n` is there to simply shut off the warning
+    // that says for unsigned types, comparison >= 0 is
+    // always true. I just used `n-n` to replace `0`.
+    // for (i = n - 1; i >= 0; i--)
+    for (i = n - 1; i >= (n - n); i--)
       _dest[i] = _src[i];
     return _dest;
   }
-  if (_src > _dest && _src - _dest < (int)n) {
+  if (_src > _dest && _src - _dest < (long)n) {
     size_t i;
     for (i = 0; i < n; i++)
       _dest[i] = _src[i];
