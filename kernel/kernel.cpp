34e795ca67192e25a4bca1dd2ea737244541b084
icebarf <sysgrammer@protonmail.com>
2025-03-20 23:47:01 +0530

    kernel: attempt to detect memory

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -1,13 +1,16 @@
+#include <cstdint>
+#include <kernel/arch/i386/mem/multiboot.h>
 #include <kernel/serial>
 #include <kernel/utils/log>
 #include <kernel/vga_tty>
+#include <stdlib.h>
 
 using comp = logger::Components;
 
 extern bool __stack_guard_initialized;
 
 extern "C" void
-kernel_main()
+kernel_main(multiboot_info_t* mbi, unsigned int mbmagic)
 {
   logger klog;
 
@@ -22,6 +25,31 @@ kernel_main()
   }
   klog.write(comp::Serial, "initialized: code: %d", v);
 
+  // Check Memory related shit
+  if (mbmagic != MULTIBOOT_BOOTLOADER_MAGIC) {
+    klog.write(comp::Kernel, "Boot magic invalid!");
+    abort();
+  }
+
+  if (!((mbi->flags >> 6) & 0x1)) {
+    klog.write(comp::Kernel,
+               "Bootloader did not pass any valid mmap information!");
+    abort();
+  }
+
+  for (unsigned int i = 0; i < mbi->mmap_length; i++) {
+    multiboot_memory_map_t* mmm_table =
+      (multiboot_memory_map_t*)(mbi->mmap_addr + i);
+    printf("Start Addr: %llx | Length: %llx | Size: %x | Type: %d\n",
+           mmm_table->addr,
+           mmm_table->len,
+           mmm_table->size,
+           mmm_table->type);
+    if (mmm_table->type == MULTIBOOT_MEMORY_AVAILABLE) {
+      printf("Memory block available!\n");
+    }
+  }
+
   if (!__stack_guard_initialized) {
     klog.write(comp::StackGuard, "uninitialized");
   }
@@ -29,5 +57,5 @@ kernel_main()
 
   klog.write(comp::Kernel, "Hello!");
 
-  klog.write(comp::Kernel, "hlt...");
+  abort();
 }
